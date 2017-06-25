026038f617373c65aae277eb3264198bd6830bc7
gentiger <gentiger@gmail.com>
2017-06-25 21:54:18 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update && apt-get upgrade -y && apt-get -y autoremove && apt-get clean
 python install.py --production
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/ /home/$FRAPPE_USER/.cache
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/ /projects/.cache
 
 exit 0
 
