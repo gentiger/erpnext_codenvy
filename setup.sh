34050c245a53e61ebd1b8606c326d011873ac5af
gentiger <gentiger@gmail.com>
2017-06-25 22:41:25 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update && apt-get upgrade -y && apt-get -y autoremove && apt-get clean
-python install.py --develop
+python install.py --production
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/ /projects/.cache
 
 exit 0
