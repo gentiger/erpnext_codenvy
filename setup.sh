482cd26e630cd8c8fc485934b9c94e30b541d8a1
gentiger <gentiger@gmail.com>
2017-06-25 18:53:05 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update && apt-get upgrade -y && apt-get -y autoremove && apt-get clean
-python install.py --develop --user $FRAPPE_USER
+python install.py --develop
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/ /projects/.cache
 
 exit 0
